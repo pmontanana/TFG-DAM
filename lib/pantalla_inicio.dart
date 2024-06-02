22295c486e7f718028b59bfdffbc9d4efcf570a5
Pablo Montañana Fuentes <pablomontanana@gmail.com>
2024-06-02 22:40:59 +0200

    Inicio del Proyecto, añadido interfaz principal en lib/main.dart y clase pantalla_inicio en lib/

---
+++ lib/pantalla_inicio.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PantallaInicio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Column(
+          children: [
+            Text("Welcome to the Home Screen"),
+          ],
+        ),
+      ),
+    );
+  }
+}

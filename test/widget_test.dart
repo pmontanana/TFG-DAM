96db3922281cbff1237ee50764668958be0b9891
Pablo Montañana <pablomontanana@gmail.com>
2026-05-20 00:57:13 +0200

    FIX: Arreglo problemas con compatibilidad iOS

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tfg/main.dart';
+import 'package:ePastillero/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
